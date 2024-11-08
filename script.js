65210d538d51c474e333cfd07434107f889971f5
NathanJW2020 <NathanJWashington2020@Gmail.com>
2024-11-08 04:10:28 +0000

    Played with JavaScript

---
+++ script.js
@@ -0,0 +1,18 @@
+let animal = "Lion"
+
+console.log("Awesome I'm now writing JavaScrpit!")
+console.log("I'm writing a second code of text!")
+console.log(animal)
+console.log("I'm still here...")
+
+//String
+let personName = "Nate"
+console.log(personName)
+
+//Number
+let purchasePrice = 15.99
+console.log("intial value purchasePrice", purchasePrice)
+
+//Boolean
+let isTuesday = false
+console.log(isTuesday)
